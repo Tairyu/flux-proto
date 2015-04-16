61255c865b2fbdf2a108acf375c9599057164f8c
清水　泰龍 <ta_shimizu@waku-2.com>
2015-04-16 21:40:27 +0900

    add: build task to babel for server resources.

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
   grunt.initConfig({
     clean: ['build'],
     concurrent: {
-      dev: ['nodemon:app', 'webpack:dev'],
+      dev: ['nodemon:app', 'webpack:dev', 'esteWatch'],
       options: {
         logConcurrentOutput: true
       }
@@ -28,12 +28,37 @@ module.exports = function(grunt) {
     },
     nodemon: {
       app: {
-        script: './server.js',
+        script: './build/server/server.js',
         options: {
-          ignore: ['build/**'],
+          ignore: ['{actions,configs,components,services,stores}/**', 'build/js/**'],
           ext: 'js,jsx'
         }
       }
+    },
+    babel: {
+      iojs: {
+        options: {
+          blacklist: ['es6.classes', 'es6.constants', 'es6.templateLiterals', 'es6.blockScoping']
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              'server.js',
+              'app.js',
+              '{actions,configs,components,services,stores}/**/*.{js,jsx}'
+            ],
+            dest: 'build/server'
+          }
+        ]
+      }
+    },
+    esteWatch: {
+      options: {
+        dirs: ['.', '{actions,configs,components,services,stores}/**/']
+      },
+      js: function(_) { return ['newer:babel:iojs']},
+      jsx: function(_) { return ['newer:babel:iojs']}
     },
     webpack: {
       dev: {
@@ -65,6 +90,6 @@ module.exports = function(grunt) {
 
   // tasks
   grunt.registerTask('lint', ['eslint:all', 'jscs:all']);
-  grunt.registerTask('default', ['clean', 'lint', 'concurrent:dev']);
+  grunt.registerTask('default', ['clean', 'lint', 'babel:iojs', 'concurrent:dev']);
 };
 
